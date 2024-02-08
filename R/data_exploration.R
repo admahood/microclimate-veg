564a4196349fc56ae82d5c3b35502ac6202ab915
Mahood <12001002933729@FEDIDCARD.GOV>
2024-02-08 15:43:49 -0700

    initial analysis coming along

--- R/data_exploration.R
+++ R/data_exploration.R
@@ -22,6 +22,8 @@ vall_plotwise_summaries <-
   vall_clim |>
   plotwise_summary()
 
+both_plotwise <- bind_rows(mef_plotwise_summaries, vall_plotwise_summaries)
+
 vall_veg <- readr::read_csv("data/valles_caldera/microclimate - VALL_veg.csv") |>
   dplyr::select(plot, species) |>
   dplyr::mutate(occurrence = 1,
@@ -47,6 +49,23 @@ mef_veg <- readr::read_csv("data/mef/manitou_species_data.csv")|>
   arrange(plot) |>
   tibble::column_to_rownames("plot");mef_veg
 
+
+both_veg <- readr::read_csv("data/valles_caldera/microclimate - VALL_veg.csv") |>
+  dplyr::select(plot, species) |>
+  bind_rows(readr::read_csv("data/mef/manitou_species_data.csv")|>
+              dplyr::select(plot, species = actual_species))|>
+  dplyr::mutate(occurrence = 1,
+                plot = str_to_lower(plot) %>% str_remove_all("_"),
+                species = str_replace_all(species, " ", "_")|>
+                  str_replace_all("-", "_")) |>
+  tidyr::pivot_wider(names_from = species, values_from = occurrence,
+                     values_fn = first, values_fill = 0) |>
+  mutate(plot = ifelse(str_sub(plot, 3,3) == "0", str_remove_all(plot, "0"), plot)) |>
+  filter(plot %in% rownames(both_plotwise)) |>
+  arrange(plot) %>%
+  tibble::column_to_rownames("plot")
+rownames(both_veg) == rownames(both_plotwise)
+
 ## topoterra ==== from THoecker
 
 terra::rast("data/big/topoterra_2C_2015.tif") -> xx
@@ -68,6 +87,14 @@ tc_val <- locations_xx |>
   dplyr::select(-ID, tc_aet = aet, tc_def = def, tc_tmx= tmax, tc_tmin = tmin) |>
   filter(str_sub(id, 1,2) == "vg") |>
   filter(id %in% rownames(vall_plotwise_summaries)) |>
+  arrange(id) |>
+  tibble::column_to_rownames("id")
+
+tc_both <- locations_xx |>
+  st_set_geometry(NULL) |>
+  dplyr::select(-ID, tc_aet = aet, tc_def = def, tc_tmx= tmax, tc_tmin = tmin) |>
+  filter(str_sub(id, 1,3) == "mef") |>
+  filter(id %in% rownames(both_plotwise)) |>
   arrange(id) |>
   tibble::column_to_rownames("id")
 
@@ -121,9 +148,9 @@ ptd <- sitesv |>
   mutate(site = "Valles Caldera") |>
   bind_rows(sites |> mutate(site = "Manitou EF")) |>
   ggplot() +
-  geom_segment(data = env_b,x=0,y=0, color = "grey",arrow = arrow(),
-               aes(yend = NMDS2, xend = NMDS1), lwd=1)+
-  geom_text_repel(data = env_b,size=4, aes(label = species, x=NMDS1, y=NMDS2), color = "grey40") +
+  # geom_segment(data = env_b,x=0,y=0, color = "grey",arrow = arrow(),
+  #              aes(yend = NMDS2, xend = NMDS1), lwd=1)+
+  # geom_text_repel(data = env_b,size=4, aes(label = species, x=NMDS1, y=NMDS2), color = "grey40") +
   geom_text(aes(x=MDS1, y=MDS2,label = plot, color = tdelta), fontface='bold') +
   scale_color_viridis_c() +
   facet_wrap(~site) +
@@ -156,3 +183,90 @@ pvm <- sitesv |>
 
 ggarrange(ptd, pvm, ncol = 1) |>
   ggsave(plot = _, filename = "out/nmds.png", bg="white", width=10, height=10)
+
+
+
+# biodiversity by tdelta ====
+div <- vall_veg |> vegan::diversity() |> as.data.frame() |> tibble::rownames_to_column("plot")
+nsp <- vall_veg |> vegan::specnumber() |> as.data.frame() |> tibble::rownames_to_column("plot")
+divm <- mef_veg |> vegan::diversity() |> as.data.frame() |> tibble::rownames_to_column("plot")
+nspm <- mef_veg |> vegan::specnumber() |> as.data.frame() |> tibble::rownames_to_column("plot")
+
+
+vall_div <- vall_plotwise_summaries |>
+  tibble::rownames_to_column("plot") |>
+  left_join(div) |>
+  left_join(nsp) |>
+  dplyr::rename(div = 14, nspp = 15)
+mef_div <- mef_plotwise_summaries |>
+  tibble::rownames_to_column("plot") |>
+  left_join(divm) |>
+  left_join(nspm) |>
+  dplyr::rename(div = 14, nspp = 15)
+
+# evidence of the range squeeze hypothesis?
+glm(nspp ~ tdelta+vmin, vall_div |> bind_rows(mef_div), family = "quasipoisson") %>% summary()
+glm(div ~ tdelta+vmin, vall_div |> bind_rows(mef_div), family = Gamma()) %>% summary()
+
+ggplot(mef_div |> bind_rows(vall_div),aes(x= tdelta, y=nspp)) +
+  geom_point()
+ggplot(mef_div |> bind_rows(vall_div),aes(x= tdelta, y=div)) +
+  geom_point()
+
+# scatterplots ============
+vall_plotwise_summaries |>
+  mutate(site = "Val") |>
+  bind_rows(mef_plotwise_summaries|>mutate(site = "mef")) |>
+  ggplot(aes(x=vmin, y=tdelta, color = site)) +
+  geom_point(size=3)
+ggsave("out/vpdmin_x_tdelta.png", width=8, height=6)
+vall_plotwise_summaries |>
+  mutate(site = "Val") |>
+  bind_rows(mef_plotwise_summaries|>mutate(site = "mef")) |>
+  lm(vmin ~ tdelta, data = _) |>
+  summary()
+
+
+# both mds
+
+vegan::metaMDS(both_veg) -> bnmds
+
+sitesb <- bnmds$points |>
+  as.data.frame() |>
+  tibble::rownames_to_column("plot") |>
+  left_join(both_plotwise |> tibble::rownames_to_column("plot"))
+
+sppb <- envfit(bnmds, both_veg, 9999)
+envb <- envfit(bnmds, both_plotwise, 9999, strata =as.factor(c(rep("mef", 12), rep("vall", 21))) )
+
+pvb <- sitesb |>
+  ggplot() +
+  geom_text(aes(label = plot, color = tdelta, x=MDS1, y=MDS2), fontface='bold') +
+  scale_color_viridis_c() +
+  coord_equal() +
+  theme_light(); pvb
+
+
+# plot for vall
+
+ptd <- sitesv |>
+  ggplot() +
+  geom_text(aes(x=MDS1, y=MDS2,label = plot, color = tdelta), fontface='bold') +
+  scale_color_viridis_c() +
+  coord_equal() +
+  theme_light(); ptd
+pvm <- sitesv |>
+  ggplot() +
+  geom_text(aes(x=MDS1, y=MDS2,label = plot, color = vmin), fontface='bold') +
+  scale_color_viridis_c() +
+  coord_equal() +
+  theme_light(); pvm
+
+ptv <- vall_plotwise_summaries |>
+  ggplot(aes(x=vmin, y=tdelta)) +
+  geom_point(size=3) +
+    theme_light()
+
+ggarrange(ptd, pvm, ncol=2 ) |>
+  ggsave(plot=_, filename = "out/vall_fig.png", width=13, height=6, bg="white")
+ggsave(plot=ptv, filename = "out/vall_fig2.png", width=5, height=5, bg="white")
--- R/jsdm.R
+++ R/jsdm.R
@@ -1,13 +1,84 @@
-vall_veg_long <- readr::read_csv("data/valles_caldera/microclimate - VALL_veg.csv") |>
+library(tidyverse)
+library(topomicro)
+library(Hmsc)
+require(snow)
+# devtools::install_github("admahood/gghmsc", force = TRUE)
+library(gghmsc)
+mef_clim <- read_csv("data/cleaned_bm/mef_jul23.csv"); glimpse(mef_clim)
+vall_clim <- read_csv("data/cleaned_bm/vall_dec22.csv"); glimpse(vall_clim)
+
+clim_summaries <-
+  mef_clim |>
+  bind_rows(vall_clim) |>
+  plotwise_summary()
+
+veg <- readr::read_csv("data/valles_caldera/microclimate - VALL_veg.csv") |>
   dplyr::select(plot, species) |>
+  bind_rows(readr::read_csv("data/mef/manitou_species_data.csv")|>
+              dplyr::select(plot, species = actual_species))|>
   dplyr::mutate(occurrence = 1,
                 plot = str_to_lower(plot) %>% str_remove_all("_"),
                 species = str_replace_all(species, " ", "_")|>
-                  str_replace_all("-", "_"))
+                  str_remove_all("\\.") |>
+                  str_replace_all("-", "_")) |>
+  tidyr::pivot_wider(names_from = species, values_from = occurrence,
+                     values_fn = first, values_fill = 0) |>
+  mutate(plot = ifelse(str_sub(plot, 3,3) == "0", str_remove_all(plot, "0"), plot)) |>
+  filter(plot %in% rownames(clim_summaries)) |>
+  arrange(plot) %>%
+  tibble::column_to_rownames("plot")
+rownames(veg) == rownames(clim_summaries)
 
-mef_veg_long <- readr::read_csv("data/mef/manitou_species_data.csv")|>
-  dplyr::select(plot, actual_species) |>
-  dplyr::mutate(occurrence = 1,
-                plot = str_to_lower(plot) %>% str_remove_all("_"),
-                actual_species = str_replace_all(actual_species, " ", "_")|>
-                  str_replace_all("-", "_"))
+prevalence <- colSums(veg) %>%
+  as_tibble(rownames = "Species") %>%
+  dplyr::rename(prevalence = value) %>%
+  arrange(desc(prevalence))
+
+ggplot(prevalence) +
+  geom_histogram(aes(x=prevalence))
+
+xformula <- formula(paste("~ site + ", paste(names(clim_summaries), collapse = " + ")))
+xdata <- clim_summaries |>
+  mutate(site = as.factor(c(rep("mef", 12), rep("val", 21))))
+
+studyDesign <- data.frame(site = as.factor(xdata$site))
+rL <- HmscRandomLevel(units = studyDesign$site)
+
+
+mod = Hmsc(Y = veg, XFormula = xformula, XData = xdata, studyDesign = studyDesign,
+           ranLevels = list("site" = rL))
+
+nChains = 4
+test.run = TRUE
+
+if (test.run){
+  #with this option, the vignette evaluates in ca. 1 minute in adam's laptop
+  thin = 10
+  samples = 100
+  transient = ceiling(thin*samples*.5)
+}else{
+  # with a spatial random effect, evaluates in 20 minutes on adam's laptop
+  thin = 10
+  samples = 1000
+  transient = ceiling(thin*samples*.5)
+}
+
+if(!dir.exists("data/hmsc"))dir.create("data/hmsc")
+t0 <- Sys.time()
+hmsc_file <- "data/hmsc/hmsc_probit_test.Rda"
+dir.create("data/hmsc")
+if(!file.exists(hmsc_file)){
+  m = sampleMcmc(mod, thin = thin,
+                 samples = samples,
+                 transient = transient,
+                 adaptNf = rep(ceiling(0.4*samples*thin),1),
+                 nChains = nChains,
+                 nParallel = nChains)
+  print(Sys.time()-t0)
+  save(m, file=hmsc_file)
+}else{load(hmsc_file)}
+
+
+gghmsc::ggplot_convergence(m)
+gghmsc::ggplot_beta(m)
+
